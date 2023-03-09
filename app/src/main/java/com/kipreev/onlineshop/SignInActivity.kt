ef9bd047aee0de4c32e7ada64e9247e826852b37
Bigy <kipreevmaksim51@gmail.com>
2023-03-09 20:54:54 +0600

    make sign in activity & login activity

--- app/src/main/java/com/kipreev/onlineshop/SignInActivity.kt
+++ app/src/main/java/com/kipreev/onlineshop/SignInActivity.kt
@@ -3,9 +3,9 @@ package com.kipreev.onlineshop
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.sigin_in_activity)
     }
 }
